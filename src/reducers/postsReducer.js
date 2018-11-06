afc93f26591c3c74aede8d7fdad394cb4bb5878f
Tanner Noble <tannernoble@gmail.com>
2018-11-06 12:33:46 -0600

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,9 +1,10 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
 } from '../actions/types';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -15,6 +16,12 @@ export default function(state = INIT_STATE, action) {
                 ...state,
                 recentPosts
             };
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state,
+                posts: resultsPosts
+            }
         default:
             return state;
     }
